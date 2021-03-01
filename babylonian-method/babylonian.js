01b4a7c825086c932d177fb59c13f7a6f51b32cb
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-03-01 00:45:40 -0600

    Started work in babylonian-method

--- babylonian-method/babylonian.js
+++ babylonian-method/babylonian.js
@@ -1,5 +1,12 @@
 const squareRoot = (radicand) => {
-  return radicand
+  var guess = radicand * 0.25;
+  console.log(radicand);
+
+  while (radicand / guess !== guess) {
+    console.log(guess);
+    var guess = (guess * (radicand / guess)) / 2;
+  }
+  return guess;
 }
 
-module.exports = squareRoot
+module.exports = squareRoot;
